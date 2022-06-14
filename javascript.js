488d61013fe7814f66ad6e69175407ab94b84fe1
josephmolner <josephmolner@gmail.com>
2022-06-14 15:12:33 -0700

    Working player selection function
    After going down a few blind alleys, I did some research and came
    across a more realistic approach to getting user input validated,
    formatted and stored in the playerSelection variable. It is a more
    pure series of functions. Previously, I was trying to fit too many
    actions into each bit of code, and encountering bug after bug.
    In addition, I added the empty functions described in the assignment,
    and lined them up in the comments to parse correctly.

--- javascript.js
+++ javascript.js
@@ -1,30 +1,63 @@
+const choices = ['rock','paper','scissors'];
+
+// Create function called game that calls "playRound"
+function game(){
+    playRound()
+}
+ // Use function playRound that takes "playerSelection" and "computerSelection" and measures their results against a hierarchy
+function playRound(){
+
+}
 
 
- /* Create function "computerPlay" to generate random selection from strings "rock", "paper", "scissors", and store in "computerSelection */
-
- function computerPlay () {
-    const choices = ['rock','paper','scissors'];
+ /* Create function "computerPlay" to generate random selection from strings "rock", "paper", "scissors" */
+function computerChoice () {
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 }
 // Create variable to store data from the computer selection: "computerSelection"
 
-const computerSelection = computerPlay(); 
+const computerSelection = computerChoice(); 
 
 // Create variable for player selection: "playerSelection", 
+//store user input in "playerSelection via prompt.
+// Convert user input to lowercase
+const playerSelection = playerChoice()
+ 
+function playerChoice() {
+    let input = prompt("Choose Rock, Paper, or Scissors.");
+    while (input == null) {
+        input = prompt("Choose Rock, Paper, or Scissors.");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input)
+    if (check == true) {
+        console.log(input)
+    }
+}
 
-const playerSelection = prompt("Make Your Choice").toLowerCase(); //store user input in "playerSelection via prompt.
- // Convert user input to lowercase
+function validateInput(choice) {
+    if (choices.includes(choice)) {
+    return true
+    } else {
+        return false
+    }
+}
+
+
+
+
+
  // Create conditional to check that user input is either "rock", "paper", or "scissors".
 
  // Prompt user for new input if not equal to "rock", "paper", or "scissors".
 
- // Use function playRound that takes "playerSelection" and "computerSelection" and measures their results against a hierarchy
+
 
  // Return a string based on each possible result, claiming user is either a winner or loser of the round
 
- // Create function called game that calls "playRound"
+ 
 
  // Loop to keep score, ending gameplay after 5 rounds. Winner is best of 5 rounds.
  
- // Output a string that declares the user a winner or loser.
+ // Output a string that declares the user a winner or loser
