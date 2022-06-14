bae8413ae8b875ea466f0697279177554c3b7032
josephmolner <josephmolner@gmail.com>
2022-06-14 15:50:27 -0700

    Updated explanitory comments.

--- javascript.js
+++ javascript.js
@@ -3,41 +3,45 @@ const choices = ['rock','paper','scissors'];
 // Create function called game that calls "playRound"
 function game(){
     playRound()
-}
- // Use function playRound that takes "playerSelection" and "computerSelection" and measures their results against a hierarchy
-function playRound(){
+    // Loop to keep score, ending gameplay after 5 rounds. Winner is best of 5 rounds.
 
 }
+ 
+function playRound(computerSelection, playerSelection){
+    // Use function playRound that takes "playerSelection" and "computerSelection" and measures their results against a hierarchy
+
+     // Return a string based on each possible result, claiming user is either a winner or loser of the round
+
+}
 
 
- /* Create function "computerPlay" to generate random selection from strings "rock", "paper", "scissors" */
+ /* Create function "computerChoice" to generate random selection from strings "rock", "paper", "scissors" */
 function computerChoice () {
     const random = Math.floor(Math.random() * choices.length);
     return choices[random];
 }
-// Create variable to store data from the computer selection: "computerSelection"
 
 const computerSelection = computerChoice(); 
 
-// Create variable for player selection: "playerSelection", 
-//store user input in "playerSelection via prompt.
-// Convert user input to lowercase
-const playerSelection = playerChoice()
+const playerSelection = playerChoice() 
  
 function playerChoice() {
-    let input = prompt("Choose Rock, Paper, or Scissors.");
-    while (input == null) {
+    let input = prompt("Choose Rock, Paper, or Scissors."); //store user input in "playerSelection via prompt.
+    while (input == null) { // null cannot be converted .tolowercase.
         input = prompt("Choose Rock, Paper, or Scissors.");
     }
-    input = input.toLowerCase();
-    let check = validateInput(input)
+    input = input.toLowerCase();  // Convert user input to lowercase
+    let check = validateInput(input) // Create conditional to check user input.
     if (check == true) {
         console.log(input)
+    } else {
+        input = prompt("Check your spelling. Choose Rock, Paper, or Scissors.");
+        
     }
 }
-
+//this function check against the original array used in computerChoice.
 function validateInput(choice) {
-    if (choices.includes(choice)) {
+    if (choices.includes(choice)) { 
     return true
     } else {
         return false
@@ -48,16 +52,14 @@ function validateInput(choice) {
 
 
 
- // Create conditional to check that user input is either "rock", "paper", or "scissors".
+ 
 
  // Prompt user for new input if not equal to "rock", "paper", or "scissors".
 
 
 
- // Return a string based on each possible result, claiming user is either a winner or loser of the round
 
  
 
- // Loop to keep score, ending gameplay after 5 rounds. Winner is best of 5 rounds.
  
  // Output a string that declares the user a winner or loser
